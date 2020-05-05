18e6e015f0f39d1ea60678679b4d8066c3357140
MOBIUS <hd2dj07@gmail.com>
2020-05-05 11:25:35 +0900

    modify shell script

--- package.sh
+++ package.sh
@@ -7,4 +7,3 @@ cd ${SCRIPTDIR}/account/
 python3 setup.py sdist bdist_wheel
 cp ${SCRIPTDIR}/account/dist/apoweroftrance-account-${VERSION}.tar.gz ${SCRIPTDIR}/radio/
 #python3 -m pip install ${SCRIPTDIR}/radio/apoweroftrance-account-${VERSION}.tar.gz
-
--- startup.sh
+++ startup.sh
@@ -22,3 +22,4 @@ cd /backend
 #done
 #uwsgi --show-config
 exec "$@"
+
